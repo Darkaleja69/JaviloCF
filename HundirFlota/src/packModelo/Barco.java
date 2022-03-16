c48544f83885fdccd39403834180708e00f970aa
aleja <alejandrogona@gmail.com>
2022-03-16 16:28:02 +0100

    X

---
+++ HundirFlota/src/packModelo/Barco.java
@@ -0,0 +1,12 @@
+package packModelo;
+
+public class Barco {
+	private int longitud;
+	private String[] coordenadas;
+	private boolean[] tocado;
+	
+	public Barco(int pLongitud) {
+		longitud=pLongitud;
+		//pedir coordenadas
+	}
+}
---
+++ HundirFlota/src/packModelo/Tablero.java
@@ -0,0 +1,10 @@
+package packModelo;
+
+public class Tablero {
+	private int[][] tableroCPU;
+	private int[][] tableroJugador;
+	private Barco[] barcosCPU;
+	private Barco[] barcosJugador;
+	
+	
+}
