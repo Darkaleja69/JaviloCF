05a7f3a6939d6de0f4610b9c73c48310ff7d7c0a
Hugo Ran <Hugo Ran@10.109.174.183>
2022-03-23 16:51:01 +0100

    Tablero

--- HundirFlota/src/packModelo/Tablero.java
+++ HundirFlota/src/packModelo/Tablero.java
@@ -1,11 +1,30 @@
 package packModelo;
 
 public class Tablero {
+	
+	//Atributos
 	private int[][] tableroCPU;
 	private int[][] tableroJugador;
 	private Barco[] barcosCPU;
 	private Barco[] barcosJugador;
 	
-	//JAA
+	private static Tablero miTablero;
+	private Casilla[][] matriz;
+	
+	//Constructora MAE
+	private Tablero() 
+	{
+		
+	}
+	public static Tablero getTablero()
+	{
+		if(miTablero == null)
+		{
+			miTablero = new Tablero();
+		}
+		return(miTablero);
+	}
+	
+	
 	
 }
