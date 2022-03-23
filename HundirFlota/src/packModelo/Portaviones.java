f1793690ae6f953f0faf9ef7a8c6b72ddd1f4844
Hugo Ran <Hugo Ran@10.109.174.183>
2022-03-23 17:27:48 +0100

    r

---
+++ HundirFlota/src/packModelo/Portaviones.java
@@ -0,0 +1,13 @@
+package packModelo;
+
+public class Portaviones extends Barco {
+	//Atributos
+	private final int longitud = 1;
+	private int x;
+	
+	//Constructora
+	public Portaviones(int pLongitud)
+	{
+		super(pLongitud);
+	}
+}
---
+++ HundirFlota/src/packModelo/Submarino.java
@@ -0,0 +1,14 @@
+package packModelo;
+
+public class Submarino extends Barco {
+	
+	//Atributos
+	private final int longitud = 1;
+	private int x;
+	
+	//Constructora
+	public Submarino(int pLongitud)
+	{
+		super(pLongitud);
+	}
+}
