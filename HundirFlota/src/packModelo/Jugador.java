38e4742b0cc4faec5c977f551a507bfb13af50b5
Hugo <Hugo@TORRE_HUGO>
2022-03-27 18:57:25 +0200

    xx

--- HundirFlota/src/packModelo/Jugador.java
+++ HundirFlota/src/packModelo/Jugador.java
@@ -1,5 +1,27 @@
 package packModelo;
 
 public class Jugador {
+	
+	//Atributos
+	private ListaBarcos listaBarc;
+	private static Jugador miJugador;
+	
+	//Constructora
+	private Jugador()
+	{
+		
+	}
+	
+	public static Jugador getJugador()
+	{
+		if(miJugador == null)
+		{
+			miJugador = new Jugador();
+		}
+		return(miJugador);
+	}
+	
+	//Métodos
+	
 
 }
