ef7bc9048651dca0fc348a7e249d1e514c59a25a
Hugo Ran <Hugo Ran@10.109.174.183>
2022-03-30 17:29:53 +0200

    x

--- HundirFlota/src/packModelo/Submarino.java
+++ HundirFlota/src/packModelo/Submarino.java
@@ -3,8 +3,6 @@ package packModelo;
 public class Submarino extends Barco {
 	
 	//Atributos
-	private final int longitud = 1;
-	private int vidas;
 	
 	//Constructora
 	public Submarino(int pLongitud)
