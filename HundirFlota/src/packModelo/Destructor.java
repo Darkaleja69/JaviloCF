65a75f4741fe538dcd6bb336c6eb6a059e5feec0
Portatil <oiertxu.ten@gmail.com>
2022-03-30 17:56:05 +0200

    jijijiju

--- HundirFlota/src/packModelo/Destructor.java
+++ HundirFlota/src/packModelo/Destructor.java
@@ -2,8 +2,7 @@ package packModelo;
 
 public class Destructor extends Barco {
 	//Atributos
-	private final int longitud = 1;
-	private int vidas;
+	
 	
 	//Constructora
 	public Destructor(int pLongitud)
--- HundirFlota/src/packModelo/Fragata.java
+++ HundirFlota/src/packModelo/Fragata.java
@@ -3,8 +3,7 @@ package packModelo;
 public class Fragata extends Barco{
 	
 	//Atributos
-	private final int longitud = 1;
-	private int vidas;
+	
 	
 	//Constructora
 	public Fragata(int pLongitud)
