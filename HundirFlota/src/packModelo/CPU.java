faebc4fd722e495ae59c38f1543b65d256ecec2d
Hugo <Hugo@TORRE_HUGO>
2022-04-18 20:29:24 +0200

    x

--- HundirFlota/src/packModelo/CPU.java
+++ HundirFlota/src/packModelo/CPU.java
@@ -7,10 +7,14 @@ import java.util.Random;
 public class CPU extends Observable{
 	private static CPU miCPU;
 	private ListaBarcos listaB;
+	private Casilla casillaAlerta;
+	private Casilla casillaSospecha;
 	
 	private CPU() 
 	{
-		listaB=new ListaBarcos();
+		listaB = new ListaBarcos();
+		casillaAlerta = null;
+		casillaSospecha = null;
 	}
 	
 	public static CPU getMiCPU() 
@@ -78,17 +82,73 @@ public class CPU extends Observable{
 	
 	public void dispararInteligente()
 	{
+		//Inicializaciones
 		ArrayList<Casilla> casillas = new ArrayList<Casilla>();
 		boolean posible = false;
 		int x = 0;
 		int y = 0;
 		Random num = new Random();
-		while(!posible) 
+		
+		//Caso 1: se ha hundido un barco o se ha disparado al agua
+		if(this.casillaAlerta == null || this.casillaAlerta.getBarco().estaHundido())
 		{
-			x = num.nextInt(10);
-			y = num.nextInt(10);
-			posible =!(Tablero.getTablero().getCasilla(x, y, true).estaTocada());
+			while(!posible) 
+			{
+				x = num.nextInt(10);
+				y = num.nextInt(10);
+				posible =!(Tablero.getTablero().getCasilla(x, y, true).estaTocada());
+			}
+			casillas = Tablero.getTablero().bombardear(x, y,true);
+			
+			//Se activa casillaAlerta
+			if(Tablero.getTablero().getCasilla(x, y, true).getBarco() != null)
+			{
+				casillaAlerta = Tablero.getTablero().getCasilla(x, y, true);
+			}
+			else
+			{
+				casillaAlerta = null;
+			}
 		}
+		//Caso 2: se ha hallado un barco por primera vez
+		else if(this.casillaAlerta.tieneBarco() && this.casillaSospecha == null)
+		{
+			boolean fila;
+			while(!posible)
+			{
+				fila = num.nextBoolean();
+				if(fila)
+				{
+					x = casillaAlerta.getFila() - 1 + num.nextInt(2);
+					y = casillaAlerta.getColumna();
+				}
+				else 
+				{
+					x = casillaAlerta.getFila();
+					y = casillaAlerta.getColumna() - 1 + num.nextInt(2);
+				}
+				posible = (!(Tablero.getTablero().getCasilla(x, y, true).estaTocada())) && (y >= 0 && x >= 0);
+			}
+			casillas = Tablero.getTablero().bombardear(x, y,true);
+			
+			//Caso 2.1 se ha dado en un barco y se ha hundido
+			if(Tablero.getTablero().getCasilla(x, y, true).getBarco() != null && Tablero.getTablero().getCasilla(x, y, true).getBarco().estaHundido())
+			{
+				casillaAlerta = null;
+			}
+			//Caso 2.2 se ha dado en un barco y no se ha hundido
+			else if(Tablero.getTablero().getCasilla(x, y, true).getBarco() != null && !(Tablero.getTablero().getCasilla(x, y, true).getBarco().estaHundido()))
+			{
+				casillaAlerta = null;
+			}
+			//Caso 2.3 no se ha dado en un barco
+			else
+			{
+				
+			}
+		}
+		
+		
 		casillas = Tablero.getTablero().bombardear(x, y,true);
 		Jugador.getJugador().enviarCasillas(casillas);
 	}
