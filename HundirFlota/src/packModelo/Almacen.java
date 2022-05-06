68dc61613212b02bf1160852f10bc055451df5b3
paula <ppintonunez@gmail.com>
2022-05-06 10:25:11 +0200

    xx

--- HundirFlota/src/packModelo/Almacen.java
+++ HundirFlota/src/packModelo/Almacen.java
@@ -1,5 +1,40 @@
 package packModelo;
 
 public class Almacen {
-
+	
+	private int cantReparaciones;
+	private int cantEscudos;
+	private int cantMisiles;
+	private int cantBombas;
+	
+	private double precioReparacion;
+	private double precioEscudo;
+	private double precioMisil;
+	private double precioBomba;
+	
+	private static Almacen miAlmacen;
+	
+	
+	private Almacen()
+	{
+			
+		cantReparaciones = 10;
+		cantEscudos = 10;
+		cantMisiles = 10;
+		cantBombas = 10;
+		precioReparacion = 10;
+		precioEscudo = 10;
+		precioMisil = 10;
+		precioBomba = 10;
+	}
+		
+	
+	public static Almacen getMiAlmacen()
+	{
+		if (miAlmacen == null)
+		{
+			miAlmacen = new Almacen();
+		}
+		return miAlmacen;
+	}
 }
