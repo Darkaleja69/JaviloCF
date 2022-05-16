060727005fce8815eb431ac855119ee003c383f2
Hugo <Hugo@TORRE_HUGO>
2022-05-16 14:41:28 +0200

    x

--- HundirFlota/src/packModelo/CasillasProhibidas.java
+++ HundirFlota/src/packModelo/CasillasProhibidas.java
@@ -19,10 +19,18 @@ public class CasillasProhibidas {
 		
 	}
 	
+	//Comprueba si se ha prohibido esa casilla
 	public boolean estaProhibida(int x, int y)
 	{
 		boolean prohibida = false;
-		
+		for(Casilla c : lista)
+		{
+			if(c.getFila() == x && c.getColumna() == y)
+			{
+				prohibida = true;
+				break;
+			}
+		}
 		return prohibida;
 	}
 
