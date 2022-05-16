f3e4dd447821b8b7c3f5ee83cc1ce7c138694b14
Hugo <Hugo@TORRE_HUGO>
2022-05-16 14:54:17 +0200

    x

--- HundirFlota/src/packModelo/CasillasProhibidas.java
+++ HundirFlota/src/packModelo/CasillasProhibidas.java
@@ -16,7 +16,29 @@ public class CasillasProhibidas {
 	//M
 	public void prohibir()
 	{
-		
+		//Primero reseteamos la lista de casillas prohibidas
+		this.lista.clear();
+		//Recorremos el tablero en busca de casillas con barcos completamente hundidos
+		for(int i = 0; i < 10; i++)
+		{
+			for(int j = 0; j < 10; j++)
+			{
+				//si encontramos una casilla con un barco hundido, prohibimos todas las casillas de alrededor
+				if(Tablero.getTablero().getCasilla(i, j, true).tieneBarco() && Tablero.getTablero().getCasilla(i, j, true).getBarco().estaHundido())
+				{
+					for(int k = i - 1; k <= i + 1; k++)
+					{
+						for(int l = j - 1; l <= j + 1; l++)
+						{
+							if(k >= 0 && k < 10 && l >= 0 && l < 10)
+							{
+								this.lista.add(Tablero.getTablero().getCasilla(i, l, true));
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 	
 	//Comprueba si se ha prohibido esa casilla
