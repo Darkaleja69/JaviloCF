2605ac90d3bdc344a0a1654f3a27f9280403e0bd
aleja <alejandrogona@gmail.com>
2022-05-17 19:05:51 +0200

    <x

--- HundirFlota/src/packModelo/Jugador.java
+++ HundirFlota/src/packModelo/Jugador.java
@@ -36,42 +36,7 @@ public class Jugador extends Observable {
 		notifyObservers(casillas);
 		return !(casillas.size() < 1);
 	}
-	
-/*	public boolean radar() {
-		//para los turnos, que el m�todo devuelva un booleano para saber si ha funcionado y q haga la CPU su movimiento
 		
-		ArrayList<Casilla> casillas = new ArrayList<Casilla>();
-		boolean radarizado = false;
-		if(this.quedanRadares()) {
-			radares = radares -1;
-			radarizado = true;
-			Random num1 = new Random();	
-			Random num2 = new Random();	
-			int pX = num1.nextInt(7)+2;
-			int pY= num2.nextInt(7)+2;
-			int fmax = pX +1;
-			int cmax = pY +1;
-			
-			Casilla c = null;
-		
-			for(int i = pX -1;i<=fmax;i++) {
-			
-				for(int j = pY -1;j<=cmax;j++) {
-				
-					c = Tablero.getTablero().getCasilla(i, j, false);
-					c.ponerRadar();
-					casillas.add(c);
-				}
-			
-			}
-		
-			CPU.getMiCPU().enviarCasillas(casillas);
-		
-		}
-	
-		return(radarizado);
-	}
-*/	
 	public boolean hayDemasiados(int pLong) {
 		return lista.hayDemasiados(pLong);
 	}
@@ -97,16 +62,6 @@ public class Jugador extends Observable {
 		return 5-(this.lista.cantidadBarcos(pLong)+pLong);
 	}
 	
-	
-/*	public void colocarEscudo(int pX, int pY)
-	{	if(miArmamento.consultaArmamento(2, dinero)) {
-		cantEscudos--;
-		ArrayList<Casilla> casillas = Tablero.getTablero().colocarEscudo(pX, pY, true);
-		Jugador.getJugador().enviarCasillas(casillas);
-		dinero=dinero-50;
-		}
-	}
-*/	
 	public int armasEnArmamento(int pOpcion) {
 		return miArmamento.armasPorUsar(pOpcion);
 	}
@@ -116,43 +71,7 @@ public class Jugador extends Observable {
 	public int radaresDisponibles() {
 		return(radares);
 	}
-	
-/*	public void comprarReparacion()
-	{
-		int precio = Almacen.getMiAlmacen().getPrecio("Reparacion");
-		if (dinero >= precio)
-		{
-			this.cantReparaciones++;
-			dinero= dinero-precio;
-		}
-	}
-*/	
-	
-	
-	//public void comprarBomba()
-	//{
-	//int precio = Almacen.getMiAlmacen().getPrecio("Bomba");
-	//if (dinero >= precio)
-	//{
-	//this.cantBombas++;
-	//dinero= dinero-precio;
-	//}
-	//}
-	
-/*	public void usarReparacion()
-	{	Casilla cas = Tablero.getTablero().getCasilla(pFila, pCol, true);
-		if (cas.tieneBarco())
-		{
-			ArrayList <Casilla> lCasillas = Tablero.getTablero().obtenerCasillasBarco(cas, true);
-			cas.getBarco().reparar();
-			for (Casilla c: lCasillas)
-			{
-				c.reparar();
-			}
-			cantReparaciones--;
-		}
-	}
-*/	
+		
 	public boolean turnoJugador(int pOpcion,int pX,int pY) {
 		boolean finTurno=false;
 			Arma miArma=miArmamento.buscarArma(pOpcion);
