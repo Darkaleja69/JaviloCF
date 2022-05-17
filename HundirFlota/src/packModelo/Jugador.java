c84de7e7cdb27d824f70133e1da8d0128c13131f
Hugo <Hugo@TORRE_HUGO>
2022-05-17 18:23:35 +0200

    x

--- HundirFlota/src/packModelo/Jugador.java
+++ HundirFlota/src/packModelo/Jugador.java
@@ -18,7 +18,7 @@ public class Jugador extends Observable {
 		radares = 1;
 		cantEscudos = 3;
 		dinero=500;
-		miArmamento=new Armamento(5);
+		miArmamento=new Armamento();
 	}
 	
 	public static Jugador getJugador() {
